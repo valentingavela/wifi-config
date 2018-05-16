72b186e240e34f293c52c65ab739e09d83b9405c
valentinsabbath <valentingavela@gmail.com>
2018-05-16 21:28:03 +0000

    commit.sh

--- cgi/find_networks.pl
+++ cgi/find_networks.pl
@@ -2,6 +2,7 @@
 use strict ;
 use warnings ;
 use CGI;
+use Encode qw(decode encode);
 
 my $cgi = new CGI ;
 
@@ -10,10 +11,13 @@ print $cgi->header('text/html');
 my $finder = qx(sudo iw dev wlan0 scan | grep SSID);
 my @networks = split(/\n/,$finder) ;
 
-# my $first = qq{<li class="refresh rotate"><button type="button">Buscar redes <svg aria-hidden="true" class="inline"><use xlink:href="img/constant.svg#refresh" /></svg></button></li>};
-# print($first) ;
+my $first = qq{<li id="find_networks" class="refresh"><button type="button" onclick="findNetworks()">Buscar redes <svg aria-hidden="true" class="inline"><use xlink:href="img/constant.svg#refresh" /></svg></button></li>};
+print($first) ;
+
 foreach my $rec (@networks)
 {
   $rec =~ s/SSID: //;
-  print (qq{<li><button type="button" data-modal="wifi">$rec</button></li>});
+  $rec =~ s/\s//g;
+  $rec = encode('UTF-8', $rec);
+  print (qq{<li><button type="button" data-modal="wifi" onclick="showForm('$rec')" >$rec</button></li>});
 }
--- cgi/set_password.pl
+++ cgi/set_password.pl
@@ -2,17 +2,23 @@
 use strict ;
 use warnings ;
 use CGI;
+# use Encode qw(decode encode);
+use utf8;
 
 my $cgi = new CGI ;
 
-my $wifi = $cgi->param('wifi') ;
+my $wifi = $cgi->param('ssid') ;
 my $pwd =  $cgi->param('pwd') ;
 
 if (length($pwd) > 7)
 {
   system("wpa_passphrase $wifi $pwd >> /home/pi/wifi-config/cli/wpa_supplicant.conf") ;
+  print $cgi->header();
+  print("ok");
+  # print $cgi->redirect('../index.html');
 }
 else
 {
-  print("pwd must 8 char min \n");
+  print $cgi->header();
+  print("La contraseña debe tener 8 caracteres como mínimo");
 }
