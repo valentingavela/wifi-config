b003ac78d458ead5cbb55a06e4b5231d7c7d3c04
valentinsabbath <valentingavela@gmail.com>
2018-05-24 18:03:43 +0000

    commit.sh

---
+++ install_modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "RUN AS SUDO!"
+apt update &&
+ln -s /home/pi/wifi-config/web/css/   /var/www/html/css
+ln -s /home/pi/wifi-config/web/img/   /var/www/html/img
+ln -s /home/pi/wifi-config/web/index2.html  /var/www/html/index2.html
+ln -s /home/pi/wifi-config/web/index.html   /var/www/html/index.html
+ln -s /home/pi/wifi-config/web/js   /var/www/html/js
+ln -s /home/pi/wifi-config/cgi/find_networks.pl   /var/www/html/cgi-bin/find_networks.pl
+ln -s /home/pi/wifi-config/cgi/set_password.pl    /var/www/html/cgi-bin/set_password.pl
+
+cp sudo/sudoers /etc/sudoers
+touch /home/pi/cfile
+echo "* * * * * perl /home/pi/wifi-config/check-internet.pl >/dev/null 2>/dev/null" >> /home/pi/cfile
+crontab /home/pi/cfiletouch /home/pi/cfile
