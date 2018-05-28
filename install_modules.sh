89897c576b6299d79aa9fb97e156e90d2c9c53b1
valentinsabbath <valentingavela@gmail.com>
2018-05-28 15:29:57 +0000

    commit.sh

--- install_modules.sh
+++ install_modules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "RUN AS SUDO!"
 apt update &&
-apt install dnsmask &&
+apt install dnsmask hostapd &&
 ln -s /home/pi/wifi-config/web/css/   /var/www/html/css
 ln -s /home/pi/wifi-config/web/img/   /var/www/html/img
 ln -s /home/pi/wifi-config/web/index2.html  /var/www/html/index2.html
